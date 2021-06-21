02bd54c2e28b603adc9971fcfa4be3de98bd86cd
João Marcos <joaomarcossm2013@hotmail.com>
2021-06-21 02:37:56 -0300

    create display component for name player

---
+++ src/components/Display.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="display">
+        {{name}}
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['name']
+}
+</script>
+
+<style>
+    .display {
+        width: 15%;
+        height: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #0004;
+        margin: 5%;
+    }
+</style>
